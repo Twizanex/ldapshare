2cd1a6973b59412e3dd484640646c3fec53e353a
Stéphane Dion <divers@sdion.net>
2012-01-15 17:53:10 +0100

    -

--- wall369.dist.php
+++ wall369.dist.php
@@ -774,7 +774,7 @@ class wall369 {
 		//$text = eregi_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)', '<a href="\\1" target="_blank"">\\1</a>', $text);
 		//$text = eregi_replace('([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_\+.~#?&//=]+)', '\\1<a href="http://\\2" target="_blank">\\2</a>', $text);
 		//$text = eregi_replace('([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})', '<a href="mailto:\\1">\\1</a>', $text);
-		return nl2br($text, 0);
+		return nl2br($text);
 	}
 	function render_datecreated($date) {
 		list($datecreated, $timecreated) = explode(' ', $date);
