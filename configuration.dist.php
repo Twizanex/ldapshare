3153630a4c3eed12ad6270d71ba9e07f0dfcfbf8
Stéphane Dion <divers@sdion.net>
2012-01-21 02:38:29 +0100

    -

--- configuration.dist.php
+++ configuration.dist.php
@@ -1,29 +1,28 @@
 <?php
-define('DATABASE_HOST', 'localhost');
-define('DATABASE_NAME', 'wall369');
-define('DATABASE_PASSWORD', 'wall369');
-define('DATABASE_PORT', 3306);
 define('DATABASE_TYPE', 'mysql');
+define('DATABASE_NAME', 'wall369');
+define('DATABASE_HOST', 'localhost');
+define('DATABASE_PORT', 3306);
 define('DATABASE_USER', 'wall369');
+define('DATABASE_PASSWORD', 'wall369');
 define('DEBUG', 0);//0, 1
 define('DEMO', 1);//0, 1
 define('GRAVATAR', 1);//0, 1
 define('GRAVATAR_DEFAULT', 'identicon');//identicon, mm, monsterid, retro, wavatar
 define('GRAVATAR_RATING', 'pg');//g, pg, r, x
 define('GZHANDLER', 1);//0, 1
-
 define('LDAP', 0);//0, 1
-define('LDAP_SERVER', 'ldap://hostname');
-define('LDAP_ADMIN_DN', 'YOURDOMAIN\\user');
-define('LDAP_ADMINPW', 'password');
-define('LDAP_DOMAINS', array('yourdomain.com'));
-define('LDAP_DN', 'DC=yourdomain,DC=com');
-define('LDAP_FILTER', 'userprincipalname=[email]');
+define('LDAP_SERVER', 'ldap://localhost');
+define('LDAP_PORT', 389);
+define('LDAP_PROTOCOL', 3);
+define('LDAP_ROOTDN', 'cn=Manager,dc=my-domain,dc=com');
+define('LDAP_ROOTPW', 'secret');
+define('LDAP_BASEDN', 'dc=my-domain,dc=com');
+define('LDAP_FILTER', 'mail=*');
 define('LDAP_LASTNAME', 'sn');
 define('LDAP_FIRSTNAME', 'givenname');
-
-define('LIMIT_COMMENTS', 5);
 define('LIMIT_POSTS', 10);
+define('LIMIT_COMMENTS', 5);
 define('TABLE_ADDRESS', 'wall369_address');
 define('TABLE_COMMENT', 'wall369_comment');
 define('TABLE_LIKE', 'wall369_like');
